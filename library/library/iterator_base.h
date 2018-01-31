2240343c92298dc86a16dc2f7369d8c03b9490e5
CanftIn <wwc7033@gmail.com>
2018-01-31 21:26:01 +0800

    update construct.h

--- library/library/iterator_base.h
+++ library/library/iterator_base.h
@@ -9,6 +9,17 @@ namespace Can
 	struct forward_iterator_tag : public input_iterator_tag {};
 	struct bidirectional_iterator_tag : public forward_iterator_tag {};
 	struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+
+	template<typename category, typename T>
+	struct iterator
+	{
+		using iterator_category = category;
+		using value_type = T;
+		using pointer = T*;
+		using reference = T&;
+		using distance = Can::ptrdiff_t;
+	};
+
 }
 
 
